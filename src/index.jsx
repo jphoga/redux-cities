46c2c3685823d74a093e9b8537833780c50cfaf6
Jan Hoga <janpaulhoga@gmail.com>
2019-08-13 23:55:35 +0900

    added dev tools

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { logger } from 'redux-logger';
 import reducers from './reducers';
+import reduxPromise from 'redux-promise';
 
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares = composeEnhancers(applyMiddleware(logger, reduxPromise));
+
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, {}, middlewares)}>
     <App />
   </Provider>,
   document.querySelector('.container')
